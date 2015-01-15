bcd21fa62c8f2d635c48eee1298fc02a868f5111
Richard Diamond <wichard@vitalitystudios.com>
2015-01-15 15:31:27 -0600

    Add a grow only set CvRDT.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 //! This is a WIP, so expect that it might change in incompatible ways.
 
 #![experimental]
+
+use std::borrow::BorrowFrom;
+use std::collections::BTreeSet;
 use std::default::Default;
 
 pub struct State<T: Default + Send, Merger: Fn(T, T) -> T> {
@@ -36,4 +39,76 @@ impl<T: Default + Send, Merger: Fn(T, T) -> T> State<T, Merger> {
         let new_state = (self.merger)(left_state, right_state);
         self.state = Some(new_state);
     }
+}
+
+
+type GOSSMerger<Atom> = fn(BTreeSet<Atom>, BTreeSet<Atom>) -> BTreeSet<Atom>;
+pub struct GrowOnlySetState<Atom: Ord + Send>(State<BTreeSet<Atom>, GOSSMerger<Atom>>);
+
+impl<Atom> GrowOnlySetState<Atom> where Atom: Ord + Send {
+    pub fn new() -> GrowOnlySetState<Atom> {
+
+        fn merger<Atom>(mut left: BTreeSet<Atom>, right: BTreeSet<Atom>) -> BTreeSet<Atom>
+            where Atom: Ord + Send
+        {
+            left.extend(right.into_iter());
+            left
+        }
+
+        GrowOnlySetState(State::new(merger))
+    }
+
+    fn inner_imm(&self) -> &State<BTreeSet<Atom>, GOSSMerger<Atom>> {
+        let &GrowOnlySetState(ref inner) = self;
+        inner
+    }
+    fn inner_mut(&mut self) -> &mut State<BTreeSet<Atom>, GOSSMerger<Atom>> {
+        let &mut GrowOnlySetState(ref mut inner) = self;
+        inner
+    }
+
+    pub fn add(&mut self, v: Atom) -> &BTreeSet<Atom> {
+        self.inner_mut()
+            .update(move |: state: &mut BTreeSet<Atom>| {
+                state.insert(v);
+            })
+    }
+
+    pub fn lookup<T: ?Sized>(&self, v: &T) -> bool where T: BorrowFrom<Atom>, T: Ord {
+        self.inner_imm()
+            .query(move |&: state: &BTreeSet<Atom>| {
+                state.contains(v)
+            })
+    }
+
+    pub fn merge(&mut self, right: BTreeSet<Atom>) {
+        self.inner_mut()
+            .merge(right)
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn grow_only_set_state() {
+        let mut left = GrowOnlySetState::new();
+        let left_state = left.add(1u64)
+            .clone();
+
+        let mut right = GrowOnlySetState::new();
+        let right_state = right.add(2u64)
+            .clone();
+
+        left.merge(right_state);
+        assert!(left.lookup(&1u64));
+        assert!(left.lookup(&2u64));
+
+        right.merge(left_state);
+        assert!(right.lookup(&1u64));
+        assert!(right.lookup(&2u64));
+    }
+
 }
